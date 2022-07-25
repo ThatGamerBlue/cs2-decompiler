24886e4150beb7984bd5ff7cdf255e2cbd9709d0
Walied K. Yassen <walied.developer@hotmail.com>
2022-07-25 11:59:29 +0300

    Fix array detection when the argument is not the first

--- src/main/java/org/runestar/cs2/dfa/FindArrayArgs.kt
+++ src/main/java/org/runestar/cs2/dfa/FindArrayArgs.kt
@@ -15,38 +15,49 @@ object FindArrayArgs : Phase.Individual() {
 
     override fun transform(f: Function, fs: FunctionSet) {
         if (f.arguments.none { it.stackType == StackType.INT }) return
+        val definedArrays =  mutableSetOf<Int>()
         for (insn in f.instructions) {
             if (insn !is Instruction.Assignment) continue
-            val e = insn.expression
-            if (e !is Expression.Operation) continue
-            val args = e.arguments.asList
-            when (e.opcode) {
+            val expr = insn.expression
+            if (expr !is Expression.Operation) continue
+            val exprArgs = expr.arguments.asList
+
+            when (expr.opcode) {
                 DEFINE_ARRAY -> {
-                    if ((args[0] as Element.Access).variable.id == 0) return
+                    definedArrays += (exprArgs[0] as Element.Access).variable.id
                 }
-                PUSH_ARRAY_INT -> {
-                    if ((args[0] as Element.Access).variable.id == 0) {
 
-                        val oldVar = f.arguments[0]
-                        val newVar = Variable.array(f.id, 0)
-                        val newVars = f.arguments.toMutableList()
-                        newVars[0] = newVar
-                        f.arguments = newVars
+                POP_ARRAY_INT, PUSH_ARRAY_INT -> {
+                    val arrayId = (exprArgs[0] as Element.Access).variable.id
+                    if (arrayId in definedArrays) continue
 
-                        val t = fs.typings.of(newVar)
-                        val ts = fs.typings.args(f.id, f.arguments.map { it.stackType }).toMutableList()
-                        ts[0] = t
-                        fs.typings.remove(oldVar)
-                        fs.typings.args[f.id] = ts
-                        transformCalls(f.id, fs)
-                        return
-                    }
+                    definedArrays += arrayId
+
+                    // Right now, we only handle the first array argument, since they do not use more
+                    // than one array, that is not problematic, we could assume argument index should match
+                    // array id to support multiple arrays, but we have no proof of such thing.
+
+                    val argIndex = f.arguments.indexOfFirst { it.stackType == StackType.INT }
+                    val oldVar = f.arguments[argIndex]
+                    val newVar = Variable.array(f.id, 0)
+                    val newVars = f.arguments.toMutableList()
+
+                    newVars[argIndex] = newVar
+                    f.arguments = newVars
+
+                    val t = fs.typings.of(newVar)
+                    val ts = fs.typings.args(f.id, f.arguments.map { it.stackType }).toMutableList()
+                    ts[argIndex] = t
+
+                    fs.typings.remove(oldVar)
+                    fs.typings.args[f.id] = ts
+                    transformCalls(f.id, fs, argIndex)
                 }
             }
         }
     }
 
-    private fun transformCalls(scriptId: Int, fs: FunctionSet) {
+    private fun transformCalls(scriptId: Int, fs: FunctionSet, argIndex: Int) {
         for (f in fs.functions.values) {
             for (insn in f.instructions) {
                 if (insn !is Instruction.Assignment) continue
@@ -56,13 +67,13 @@ object FindArrayArgs : Phase.Individual() {
 
                 // todo
                 val oldArgs = e.arguments.asList
-                val oldArg = oldArgs[0] as Element.Access
+                val oldArg = oldArgs[argIndex] as Element.Access
                 val newArgs = oldArgs.toMutableList()
                 val newArg = Element.Pointer(Variable.array(f.id, 0))
-                newArgs[0] = newArg
+                newArgs[argIndex] = newArg
                 e.arguments = Expression(newArgs)
                 if (f.id != scriptId) {
-                    assign(fs.typings.of(newArg), fs.typings.args.getValue(scriptId)[0])
+                    assign(fs.typings.of(newArg), fs.typings.args.getValue(scriptId)[argIndex])
                 }
             }
         }
