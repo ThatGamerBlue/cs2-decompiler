4cbabc4184148d54cf50fad9670d8f43bd7f0b9a
Joshua Filby <joshua@filby.me>
2022-11-17 03:23:41 -0600

    Make `decompile` return the FunctionSet

--- src/main/java/org/runestar/cs2/Decompiler.kt
+++ src/main/java/org/runestar/cs2/Decompiler.kt
@@ -5,14 +5,16 @@ import org.runestar.cs2.cfa.reconstruct
 import org.runestar.cs2.cg.Generator
 import org.runestar.cs2.dfa.Phase
 import org.runestar.cs2.ir.Command
+import org.runestar.cs2.ir.FunctionSet
 import org.runestar.cs2.ir.interpret
 import org.runestar.cs2.util.Loader
 
 fun decompile(
         scripts: Loader.Keyed<Script>,
         generator: Generator,
-) {
+): FunctionSet {
     val fs = interpret(scripts, Command.LOADER, PARAM_TYPES, DBTABLE_TYPES)
     Phase.DEFAULT.transform(fs)
     fs.functions.values.forEach { generator.write(it, fs, reconstruct(it)) }
+    return fs
 }
